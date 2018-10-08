86033cae003790284c99804188414199fdcd70c3
ShutoDev <dscx1490@gmail.com>
2018-10-08 23:55:25 +0900

    heroku用の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
 	def configure_permitted_parameters
-	    added_attrs = [ :username, :email, :address, :password, :chatwork_token,:password_confirmation]
+	    added_attrs = [ :name, :username, :email, :address, :password, :chatwork_token,:password_confirmation]
 	    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
 	    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
 	    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
