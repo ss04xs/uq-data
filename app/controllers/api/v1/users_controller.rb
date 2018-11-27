fba73c2448374b391b00fcee5527e52a2ea60dd8
ShutoDev <dscx1490@gmail.com>
2018-11-28 03:12:36 +0900

    build

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,7 +13,15 @@ class Api::V1::UsersController < ApplicationController
 	require 'json'
   	me = params[:message]
   	amount = params[:amount]
-  	me_format = "[info][title]有給申請[/title]日時: x月x日 何日何時間\n送金:#{amount} soda　送金日時:x月x日x時[/info] #{me}"
+  	#有給時間の計算
+    uq_x = amount / 800.to_f
+    uq_fract_m = uq_x * 10 % 10 /10
+    uq_fract_m2 = (uq_fract_m * 800 / 100.to_f) * 10 % 10 /10.to_f
+    uq_fract_m3 = (uq_fract_m2 * 100).to_s.split(".")[0]
+    uq_day = uq_x.to_s.split(".")[0]+"日"
+    uq_hours = (uq_fract_m * 800 / 100.to_f).to_s.split(".")[0]+"時間"
+    uq_minutes = (uq_fract_m3.to_f/100 * 60).to_s.split(".")[0]+"分"
+  	me_format = "[info][title]有給申請[/title]日時: x月x日 #{uq_day}#{uq_hours}#{uq_minutes}\n送金:#{amount} soda　送金日時:x月x日x時[/info] #{me}"
   	logger.debug params[:address]
   	uri = URI.parse("https://api.chatwork.com/v2/rooms/#{user.room_id}/messages")
 	request = Net::HTTP::Post.new(uri)
